c965445c1b89e0025a510d03467b898012823cb5
tuansdf <103062682+tuansdf@users.noreply.github.com>
2023-06-05 13:18:32 +0700

    feat: add register page

--- src/features/auth/components/register-form.tsx
+++ src/features/auth/components/register-form.tsx
@@ -1,3 +1,84 @@
+import { AtSymbolIcon, KeyIcon } from "@heroicons/react/20/solid";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Box, Button, TextInput } from "@mantine/core";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { z } from "zod";
+
+const formSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8).max(64),
+});
+
+type FormValues = z.infer<typeof formSchema>;
+
+const defaultValues: FormValues = {
+  email: "",
+  password: "",
+};
+
 export const RegisterForm = () => {
-  return <div>RegisterForm</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>({
+    defaultValues,
+    resolver: zodResolver(formSchema),
+  });
+
+  const onSubmit: SubmitHandler<FormValues> = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <Box
+      component="form"
+      sx={{
+        width: "100%",
+      }}
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <TextInput
+        placeholder="name@example.com"
+        type="email"
+        label="Email"
+        error={errors.email?.message}
+        withAsterisk
+        icon={
+          <Box
+            component={AtSymbolIcon}
+            sx={(theme) => ({
+              width: theme.fontSizes.md,
+              height: theme.fontSizes.md,
+            })}
+          />
+        }
+        {...register("email")}
+      />
+      <TextInput
+        placeholder="••••••••"
+        type="password"
+        label="Password"
+        error={errors.password?.message}
+        withAsterisk
+        icon={
+          <Box
+            component={KeyIcon}
+            sx={(theme) => ({
+              width: theme.fontSizes.md,
+              height: theme.fontSizes.md,
+            })}
+          />
+        }
+        sx={(theme) => ({ marginTop: theme.spacing.xs })}
+        {...register("password")}
+      />
+      <Button
+        type="submit"
+        sx={(theme) => ({ marginTop: theme.spacing.lg, width: "100%" })}
+      >
+        Register
+      </Button>
+    </Box>
+  );
 };
--- src/pages/auth/register/register-page.tsx
+++ src/pages/auth/register/register-page.tsx
@@ -1,3 +1,35 @@
+import { Anchor, Box, Text, Title } from "@mantine/core";
+import { Link } from "react-router-dom";
+import { RegisterForm } from "~/features/auth";
+
 export const RegisterPage = () => {
-  return <div>RegisterPage</div>;
+  return (
+    <Box
+      sx={{
+        maxWidth: "20rem",
+        width: "100%",
+      }}
+    >
+      <Title
+        order={2}
+        sx={(theme) => ({
+          marginBottom: theme.spacing.sm,
+        })}
+      >
+        Register
+      </Title>
+      <RegisterForm />
+      <Text
+        sx={(theme) => ({
+          marginTop: theme.spacing.xs,
+          fontSize: theme.fontSizes.sm,
+        })}
+      >
+        Have an account?{" "}
+        <Anchor component={Link} to="/login">
+          Login
+        </Anchor>
+      </Text>
+    </Box>
+  );
 };
